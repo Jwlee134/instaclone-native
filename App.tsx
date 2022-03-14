7c62d4e360788702a42184a1db6e6a63b178c3e3
Jaewon <sorhd134@gmail.com>
2022-03-14 11:35:19 +0900

    AppLoading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
+import AppLoading from "expo-app-loading";
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
 
 const App = () => {
-  return <SafeAreaView></SafeAreaView>;
+  const [loaded] = useFonts(Ionicons.font);
+
+  if (!loaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <SafeAreaView>
+      <Text>Haha</Text>
+    </SafeAreaView>
+  );
 };
 
 export default App;
