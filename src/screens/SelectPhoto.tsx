b06888919229ecf5b1c393a0568cf2b714b2e57a
Jaewon <sorhd134@gmail.com>
2022-03-24 14:14:33 +0900

    Select Photo part Two

--- src/screens/SelectPhoto.tsx
+++ src/screens/SelectPhoto.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styled from "styled-components/native";
+import * as MediaLibrary from "expo-media-library";
+import { Alert } from "react-native";
 
 const Container = styled.View`
   flex: 1;
@@ -14,6 +16,30 @@ const Bottom = styled.View`
 `;
 
 const SelectPhoto = () => {
+  const [photos, setPhotos] = useState<string[]>([]);
+
+  const getPhoto = async () => {
+    const { assets } = await MediaLibrary.getAssetsAsync();
+    setPhotos(assets.map(asset => asset.uri));
+  };
+
+  const getPermission = useCallback(async () => {
+    const { status, canAskAgain } = await MediaLibrary.getPermissionsAsync();
+    if (status === "denied" && !canAskAgain) {
+      Alert.alert(
+        "Go to the application setting and allow permission manually.",
+      );
+    } else if (status === "granted") {
+      await getPhoto();
+    } else {
+      await MediaLibrary.requestPermissionsAsync();
+    }
+  }, []);
+
+  useEffect(() => {
+    getPermission();
+  }, [getPermission]);
+
   return (
     <Container>
       <Top></Top>
