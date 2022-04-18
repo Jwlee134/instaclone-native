de0ebeea2853827d261ddba6c6a7ebe14568c509
Jaewon <sorhd134@gmail.com>
2022-04-18 20:08:06 +0900

    Room Screen part Two

--- src/screens/Room.tsx
+++ src/screens/Room.tsx
@@ -1,7 +1,38 @@
 import React, { useLayoutEffect } from "react";
-import { Text, View } from "react-native";
+import { FlatList, KeyboardAvoidingView, Platform } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import styled from "styled-components/native";
+import { DEFAULT_AVATAR } from "../apollo";
+import ScreenLayout from "../components/ScreenLayout";
 import { useSeeRoomQuery } from "../graphql/generated";
 import { RoomScreenProps } from "../types/navigators";
+
+const MessageContainer = styled.View``;
+
+const Author = styled.View``;
+
+const Avatar = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+`;
+
+const Username = styled.Text`
+  color: white;
+`;
+
+const Message = styled.Text`
+  color: white;
+`;
+
+const Input = styled.TextInput`
+  background-color: white;
+  height: 40px;
+  padding: 0 20px;
+  border-radius: 1000px;
+  width: 95%;
+  margin: 0 auto;
+`;
 
 const Room = ({
   navigation,
@@ -9,18 +40,46 @@ const Room = ({
     params: { id, title },
   },
 }: RoomScreenProps) => {
+  const bottom = useSafeAreaInsets().bottom;
+
   useLayoutEffect(() => {
     navigation.setOptions({ title });
   }, [navigation, title]);
 
-  const { data: { seeRoom } = {} } = useSeeRoomQuery({
+  const { data: { seeRoom } = {}, loading } = useSeeRoomQuery({
     variables: { seeRoomId: id },
   });
-  console.log(seeRoom);
+
   return (
-    <View>
-      <Text>Room</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "android" ? undefined : "height"}
+      keyboardVerticalOffset={
+        Platform.OS === "android" ? undefined : 40 + bottom
+      }>
+      <ScreenLayout loading={loading}>
+        <FlatList
+          style={{ paddingBottom: bottom }}
+          data={seeRoom?.messages || []}
+          renderItem={({ item }) => (
+            <MessageContainer>
+              <Author>
+                <Avatar source={{ uri: item?.user.avatar || DEFAULT_AVATAR }} />
+                <Username>{item?.user.username}</Username>
+              </Author>
+              <Message>{item?.text}</Message>
+            </MessageContainer>
+          )}
+          keyExtractor={item => item?.id + ""}
+        />
+        <Input
+          style={{ marginBottom: bottom }}
+          placeholder="Write a message..."
+          placeholderTextColor="gray"
+          returnKeyType="send"
+        />
+      </ScreenLayout>
+    </KeyboardAvoidingView>
   );
 };
 
